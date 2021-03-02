7eed905d5c6d2fd7af91da1f05564680d7299519
Eoin Oliver <eoin@squareballoon.co.uk>
2021-03-02 21:36:35 +0000

    add custom.css

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ $component = ($this->params->get('mainoutput'));
 
 $doc->addStyleSheet('https://unpkg.com/purecss@2.0.5/build/pure-min.css',array('integrity' => 'sha384-LTIDeidl25h2dPxrB2Ekgc9c7sEC3CWGM6HeFmuDNUjX76Ert4Z4IY714dhZHPLd', 'crossorigin' => 'anonymous'));
 $doc->addStyleSheet('https://unpkg.com/purecss@2.0.5/build/grids-responsive-min.css');
+$doc->addStyleSheet('css/custom.css');
 
 
 $debug           = ($this->countModules('debug'));
