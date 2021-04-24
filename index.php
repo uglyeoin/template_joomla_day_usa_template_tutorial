52d69e317cb3b1b4a0cbccd771edac3a95f8df03
Eoin Oliver <eoin@squareballoon.co.uk>
2021-04-24 18:47:45 +0100

    add html helper

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 defined('_JEXEC') or die;
 
 use \Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 
 $app             = Factory::getApplication();
 $doc             = Factory::getDocument();
@@ -36,8 +37,8 @@ $year     = JFactory::getDate()->format('Y');
 /* Component? */
 $component = ($this->params->get('mainoutput'));
 
-$doc->addScript('https://unpkg.com/purecss@2.0.5/build/pure-min.css',array('integrity' => 'sha384-LTIDeidl25h2dPxrB2Ekgc9c7sEC3CWGM6HeFmuDNUjX76Ert4Z4IY714dhZHPLd', 'crossorigin' => 'anonymous'));
-
+/* Pure CSS Grids */
+HTMLHelper::_('stylesheet', 'https://unpkg.com/purecss@2.0.5/build/grids-responsive-min.css', ['version' => 'auto', 'relative' => true, 'crossorigin' => 'anonymous']);
 
 $debug           = $this->countModules('debug');
 $navigation      = $this->countModules('navigation');
